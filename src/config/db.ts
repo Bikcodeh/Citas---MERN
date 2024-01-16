1e640435519ddac410033edf9f0edfba48b941e4
Bikcode <bikcodeh@gmail.com>
2024-01-15 22:37:35 -0500

    create connection for mongo and create .env file

---
+++ src/config/db.ts
@@ -0,0 +1,13 @@
+import mongoose, { MongooseError, Mongoose } from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.MONGO_URI || '');
+        console.log(`DB Connected to: ${connection.connection.host} on port: ${connection.connection.port}`);
+    } catch (error) {
+        console.log('Error trying to connect DB: ', error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
