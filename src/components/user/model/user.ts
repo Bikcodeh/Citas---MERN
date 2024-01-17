a750e987c40ff0376360e7952c1eb94a233c39aa
Bikcode <bikcodeh@gmail.com>
2024-01-17 09:46:08 -0500

    implements encrypt password

--- src/components/user/model/user.ts
+++ src/components/user/model/user.ts
@@ -1,5 +1,6 @@
 import { IUser } from '../interface/index';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema<IUser>({
     name: {
@@ -27,6 +28,14 @@ const userSchema = new mongoose.Schema<IUser>({
     }
 }, {
     timestamps: true
+});
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 const User = mongoose.model<IUser>("User", userSchema);
