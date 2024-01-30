2e91f656d43817ffea070ac76b0b2a13d1a0039f
Bikcode <bikcodeh@gmail.com>
2024-01-30 15:42:02 -0500

    partial creation for controller and routes for tasks

---
+++ src/components/tasks/infraestructure/controller/tasks.controller.ts
@@ -0,0 +1,22 @@
+import { injectable } from 'inversify';
+
+@injectable()
+export class TasksController {
+    constructor() {}
+
+    public addTask = async (req: Request, res: Response) => {
+
+    }
+
+    public EditTask = async (req: Request, res: Response) => {
+        
+    }
+
+    public deleteTask = async (req: Request, res: Response) => {
+        
+    }
+
+    public getTask = async (req: Request, res: Response) => {
+        
+    }
+}
